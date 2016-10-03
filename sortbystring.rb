ebe994f2fec8173782c5ee3b8c0793e4389d00ab
Vannak Chhay <vannak.chhay1@gmail.com>
2016-10-03 16:45:53 -0500

    sort_by_string

---
+++ sortbystring.rb
@@ -0,0 +1,9 @@
+# Sort the letters in the string s by the order they occur in the string t.
+#
+# Example
+#
+# For s = "weather" and t = "therapyw", the output should be
+# sortByString(s, t) = "theeraw";
+#
+# For s = "good" and t = "odg", the output should be
+# sortByString(s, t) = "oodg".
