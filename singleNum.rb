fc85386ce34109303eee39cdddeb54d724834f35
Vannak Chhay <vannak.chhay1@gmail.com>
2016-10-03 16:42:53 -0500

    single Number

---
+++ singleNum.rb
@@ -0,0 +1,6 @@
+# You are given an array of integers in which every element appears twice, except for one. Find the element that only appears one time. Your solution should have a linear runtime complexity (O(n)). Try to implement it without using extra memory.
+#
+# Example
+#
+# For nums = [2, 2, 1], the output should be
+# singleNumber(nums) = 1.
