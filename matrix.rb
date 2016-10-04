e23e6becc18fd9692e492e417855d6393dfda4bb
Vannak Chhay <vannak.chhay1@gmail.com>
2016-10-03 16:48:53 -0800

    matrix

---
+++ matrix.rb
@@ -0,0 +1,15 @@
+# Given a rectangular matrix, return all of the elements of the matrix in spiral order.
+#
+# Example
+#
+# For
+#
+# matrix =
+# [
+#  [ 1, 2, 3 ],
+#  [ 8, 9, 4 ],
+#  [ 7, 6, 5 ]
+# ]
+# the output should be
+# matrixElementsInSpiralOrder(matrix) = [1, 2, 3, 4, 5, 6, 7, 8, 9].
+#
