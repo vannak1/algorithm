478f751bc79eb7471474d7ef588b23bc40834eb7
Vannak Chhay <vannak.chhay1@gmail.com>
2016-10-16 21:48:56 -0400

    added comments to stock.rb

--- stock.rb
+++ stock.rb
@@ -1,11 +1,17 @@
 def find_max_profit(arr_stock_prices)
+   #initialize loop count to save execution time
    loop_iteration = arr_stock_prices.length - 1
    max_profit = 0
 
    loop_iteration.times do |i|
+     #returned value from arr set to buy_price and arr is shifted to the right
      buy_price = arr_stock_prices.shift
+
+     #takes the buy price that was popped off and subtracts all numbers after
      arr_stock_prices.each do |j|
        potential_profit = buy_price - j
+
+       #compares each difference to max_profit to find the greater
        max_profit = [potential_profit, max_profit].max
      end
     end
