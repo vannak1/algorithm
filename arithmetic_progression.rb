3ccc5f0010d64b06a1056e31e8fa10a4e5febac7
Vannak Chhay <vannak.chhay1@gmail.com>
2016-10-30 10:21:25 -0400

    added iteration to go over each element

--- arithmetic_progression.rb
+++ arithmetic_progression.rb
@@ -6,21 +6,37 @@
 # array_of_inputs = []
 # num_of_inputs.times{ array_of_inputs.push(gets.chomp.to_i)}
 
-  #create all possible arrangements
-  #checks if difference is constant
-def combo(arr_of_inputs, num_of_inputs)
-  result = []
-  #loop through n-1 times
-  num_of_inputs - 1.times do |i|
-    temp = []
-
-    #iterate through all 0 and 2 length
-    arr_of_inputs.each {|el| temp.push(el)}
-    result << temp
-    #on 3rd iteration check if difference is constant then push
-  end
-
-  puts result
+def factorial(n)
+  return 1 if n < 1
+  n * factorial(n-1)
 end
 
-combo([2,4,2], 3)
+def ncr(n, r)
+  factorial(n) / (factorial(r) * factorial(n-r))
+end
+
+def count_progression(n, array)
+    #adds for empty set, single set, and doubles
+    total = 1 + n + ncr(3,2)
+    copy_array = array
+    #iterates through every single element
+    array.each_with_index do |el, i|
+      #find difference between el and all other el in array.
+      diff = []
+      iteration = array.length - 1 - i
+      puts "outside #{el} index: #{i}"
+
+      iteration.times do |j|
+        diff << (array[i+j+1] - el).abs
+        puts "inside #{j} element: #{el} array_el: #{array[i+j]} diff: #{diff}"
+      end
+
+      #count how many times a constant difference appears
+      #iterates through the difference array.  finds all similar diff, adds to total
+      diff.each do |el|
+
+      end
+    end
+end
+
+count_progression(10, [18,44,66,73,91,9,90,49,64,42])
