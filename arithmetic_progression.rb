5a2e9e9ea5156facfbd6128ed951a8312a7b1a9e
Vannak Chhay <vannak.chhay1@gmail.com>
2016-10-29 20:38:11 -0400

    changed to recursion to add all arrangements

--- arithmetic_progression.rb
+++ arithmetic_progression.rb
@@ -1,23 +1,12 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-num_of_inputs = gets.chomp.to_i
-
-def factorial(n)
-  return 1 if n < 1
-  n * factorial(n-1)
-end
-
-def ncr(n, r)
-  factorial(n) / (factorial(r) * factorial(n-r))
-end
-
-def calc_sub(num_input)
-  count = 0
-
-  num_input.times do |i|
-    count += ncr(num_input, i + 1)
-  end
-
-  count
-end
-
-calc_sub(num_of_inputs)
+function getCombinations(chars) {
+  var result = [];
+  var f = function(prefix, chars) {
+    for (var i = 0; i < chars.length; i++) {
+      result.push(prefix + chars[i]);
+      f(prefix + chars[i], chars.slice(i + 1));
+    }
+  }
+  f('', chars);
+  return result;
+}
