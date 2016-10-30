15fa34e4531c789645ab9946f7f5003bbbd8e411
Vannak Chhay <vannak.chhay1@gmail.com>
2016-10-29 20:54:13 -0400

    changed logic of solution

--- arithmetic_progression.rb
+++ arithmetic_progression.rb
@@ -1,22 +1,19 @@
+#input [2,4,2]
+#output  {}, {2}, {4}, {2}, {2, 4}, {4,2}, {2, 2}
+
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-function getCombinations(chars) {
-  var result = [];
-  var f = function(prefix, chars) {
-    for (var i = 0; i < chars.length; i++) {
-      result.push(prefix + chars[i]);
-      f(prefix + chars[i], chars.slice(i + 1));
-    }
-  }
-  f('', chars);
-  return result;
-}
+num_of_inputs = gets.chomp.to_i
+array_of_inputs = []
+num_of_inputs.times{ array_of_inputs.push(gets.chomp.to_i)}
 
-def get_combination(array)
-  result = []
+#create all possible arrangements
+#checks if difference is constant
 
+#loop through n-1 times
+num_of_inputs - 1.times do |i|
+  temp = []
 
+  #iterate through all 0 and 2 length
+
+  #on 3rd iteration check if difference is constant then push
 end
-
-def sequencer(array)
-  array.each do |el|
-    result.push(prefix + el)
