b2512c0456213f77ba67ab17847c2fb1bf13cb0f
Vannak Chhay <vannak.chhay1@gmail.com>
2016-11-27 15:34:53 -0500

    subset sum

---
+++ subset_sum.rb
@@ -0,0 +1,11 @@
+# Given an array of numbers arr, determine whether arr can be divided into two subsets for which the sum of elements in both subsets is the same.
+#
+# Example
+#
+# For arr = [3, 5, 16, 8], the output should be
+# subsetSum(arr) = true.
+#
+# It is possible to partition this array into two subsets that have a sum of 16: [16] and [3, 5, 8].
+#
+# For arr = [5, 7, 1], the output should be
+# subsetSum(arr) = false.
