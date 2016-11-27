2aa85b9c927e6a1830fb585a53ae1c8cb7f191bf
Vannak Chhay <vannak.chhay1@gmail.com>
2016-11-27 16:42:53 -0500

    sudoku

---
+++ sudoku.rb
@@ -0,0 +1,3 @@
+# Sudoku is a number-placement puzzle. The objective is to fill a 9 × 9 grid with numbers in such a way that each column, each row, and each of the nine 3 × 3 sub-grids that compose the grid all contain all of the numbers from 1 to 9 one time.
+#
+# Implement an algorithm that will check whether the given grid of numbers represents a valid Sudoku puzzle according to the layout rules described above. Note that the puzzle represented by grid does not have to be solvable.
